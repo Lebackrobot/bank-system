dcc34ee79deec58619f9d4eabb5f2d255fa12989
Pedro Fernandes <dr.pedrofernades@gmail.com>
2022-08-03 18:52:44 -0300

    doc: driven code

--- main.cpp
+++ main.cpp
@@ -1,18 +1,55 @@
 #include "library/library.cpp"
 using namespace std;
 
-
+//drivem code
 int main() {
 
     Account account("Account 1", 100);
     Client client;
 
-    client.creditToAccount(account, 100);
-    client.creditToAccount(account, 100);
-    client.creditToAccount(account, 100);
+    //debit clients
+    thread client_debitToAccount1(_debitToAccount, "clientDebit 1", &account, getRandonNumber());
+    thread client_debitToAccount2(_debitToAccount, "clientDebit 2", &account, getRandonNumber());
+    thread client_debitToAccount3(_debitToAccount, "clientDebit 3", &account, getRandonNumber());
+    thread client_debitToAccount4(_debitToAccount, "clientDebit 4", &account, getRandonNumber());
+    thread client_debitToAccount5(_debitToAccount, "clientDebit 5", &account, getRandonNumber());
+    
+    //credit cleints
+    /* thread client_creditToAccount1(_creditToAccount, "cleintCredit 1", &account, getRandonNumber());
+    thread client_creditToAccount2(_creditToAccount, "clientCreditAccount 2", &account, getRandonNumber());
+    thread client_creditToAccount3(_creditToAccount, "clientCreditAccount 3", &account, getRandonNumber());
+    thread client_creditToAccount4(_creditToAccount, "clientCreditAccount 4", &account, getRandonNumber());
+    thread client_creditToAccount5(_creditToAccount, "clientCreditAccount 5", &account, getRandonNumber()); */
 
-    client.getExtract(account);
+    //thread threadCredit(_creditToAccount, &account, 500);
+    //thread threadDebit(_debitToAccount, &account, 600);
 
+    //threadCredit.join(); 
+    //threadDebit.join();
+
+    
+
+    /* thread client_getExtract1(_getAccountExtract, "client 1", &account);
+    thread client_getExtract2(_getAccountExtract, "client 2", &account);
+    thread client_getExtract3(_getAccountExtract, "client 3", &account);
+    thread client_getExtract4(_getAccountExtract, "client 4", &account);
+    thread client_getExtract5(_getAccountExtract, "client 5", &account);
+    thread client_getExtract6(_getAccountExtract, "client 6", &account);
+    thread client_getExtract7(_getAccountExtract, "client 7", &account);
+    thread client_getExtract8(_getAccountExtract, "client 8", &account);
+    thread client_getExtract9(_getAccountExtract, "client 9", &account);
+    thread client_getExtract10(_getAccountExtract, "client 10", &account); */
+
+    /* client_getExtract1.join();
+    client_getExtract2.join();
+    client_getExtract3.join();
+    client_getExtract4.join();
+    client_getExtract5.join();
+    client_getExtract6.join();
+    client_getExtract7.join();
+    client_getExtract8.join();
+    client_getExtract9.join();
+    client_getExtract10.join(); */
     return 0; 
 }
 
